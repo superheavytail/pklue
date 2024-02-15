257b359ef978ede93d3c2f9a18312fab899c9a00
heavytail <k0s1k0s1k0@korea.ac.kr>
2024-02-15 16:22:55 +0900

    release: 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pklue",
-    version="0.2.1",
+    version="0.2.2",
     description="Korean Datasets for Instruction Tuning",
     packages=find_packages(),
     author="Jeongwook Kim",
