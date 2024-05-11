0972034383606d3f44b794cdc9c7eeb31ad8db16
heavytail <k0s1k0s1k0@korea.ac.kr>
2024-05-11 23:37:54 +0900

    keep refactoring: kobest_copa

---
+++ pklue/available_dataset/kobest_copa/processor.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+from datasets import load_dataset
+import yaml
+
+from ...utils import convert_to_chat, _make_options_str, make_random_template_data
+
+
+def process(max_examples, split):
+    ds = load_dataset('skt/kobest_v1', 'copa', split=split)
+
+    if max_examples:
+        ds = ds.train_test_split(train_size=max_examples)['train']
+
+    # add [options, euro_or_ro(으로/로), eun_or_neun(은/는), answer] columns to dataset
+    def adding_columns(example):
+        options = _make_options_str(
+            example['alternative_1'], example['alternative_2']
+        )
+        question = example['question'].strip()  # since it is occasionally not stripped
+        if question == '원인':
+            euro_or_ro = '으로'
+            eun_or_neun = '은'
+        elif question == '결과':
+            euro_or_ro = '로'
+            eun_or_neun = '는'
+        else:
+            raise NotImplementedError(f"unexpected raw data question: '{example['question']}'")
+        answer = example[f"alternative_{example['label'] + 1}"]
+        return {
+            "options": options,
+            "euro_or_ro": euro_or_ro,
+            "eun_or_neun": eun_or_neun,
+            "answer": answer
+        }
+    new_ds = ds.map(adding_columns)
+
+    with open(Path(__file__).parent / "template_copa.yaml", 'rt', encoding='utf-8') as f:
+        templates = yaml.load(f, Loader=yaml.BaseLoader)['kobest_copa']
+    new_ds = make_random_template_data(templates, new_ds, max_examples)
+
+    new_ds = convert_to_chat(new_ds)
+
+    return new_ds
