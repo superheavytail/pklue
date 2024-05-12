d877487eaab53e5dab5d439c8ae9680058333023
heavytail <k0s1k0s1k0@korea.ac.kr>
2024-05-12 13:51:12 +0900

    keep refactoring: pseudo_commongen

---
+++ pklue/available_dataset/pseudo_commongen/processor.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+import yaml
+
+from ...utils import make_random_template_data, convert_to_chat, load_dataset_max_examples
+
+
+def process(max_examples, split):
+    ds = load_dataset_max_examples("heavytail/ko_commongenv2", split, max_examples)
+
+    with open(Path(__file__).parent / "template.yaml", 'rt', encoding='utf-8') as f:
+        templates = yaml.load(f, Loader=yaml.BaseLoader)['pseudo_commongen']
+
+    new_ds = make_random_template_data(templates, ds)
+    new_ds = convert_to_chat(new_ds)
+    return new_ds
