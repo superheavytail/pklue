c11aa54f11cf70acb2adacc20d79f125eb7350f4
superheavytail <k0s1k0s1k0@korea.ac.kr>
2024-08-27 15:03:57 +0900

    typo

--- pklue/available_dataset/neutral_alpaca_gpt4_ko/processor.py
+++ pklue/available_dataset/neutral_alpaca_gpt4_ko/processor.py
@@ -2,7 +2,7 @@ from ...utils import convert_to_chat, load_dataset_max_examples
 
 
 def process(max_examples, split):
-    ds = load_dataset_max_examples("heavytail/neutral-alpaca-gpt4-ko", split, max_examples)
+    ds = load_dataset_max_examples("heavytail/neutral_alpaca_gpt4_ko", split, max_examples)
 
     # change 'instruction', 'output' column names to 'user', 'assistant' and make it dictionary form
     ds = ds.rename_columns({
