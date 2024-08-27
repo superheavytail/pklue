49b3738b9ff1fdd0d458ca8384dcaf784cb2549c
superheavytail <k0s1k0s1k0@korea.ac.kr>
2024-08-27 15:27:46 +0900

    fix: duplicated preprocessing in Uracle dataset

--- pklue/available_dataset/neutral_korean_multiturn_gpt4_kullm/processor.py
+++ pklue/available_dataset/neutral_korean_multiturn_gpt4_kullm/processor.py
@@ -1,8 +1,10 @@
 from datasets import concatenate_datasets, load_dataset
 
+from ...utils import load_dataset_max_examples
+
 
 def process(max_examples, split):
-    ds = load_dataset('heavytail/neutral_korean_multiturn_gpt4_kullm')
+    ds = load_dataset_max_examples('heavytail/neutral_korean_multiturn_gpt4_kullm', split, max_examples)
 
     # concatenate ultrachat, aha, hand
     # ds = concatenate_datasets([ds['ultrachat'], ds['aha'], ds['hand']])
